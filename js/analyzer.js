c25a0d0e4d807c79c591a757adb9717392272bcd
nicasnes <31024845+nicasnes@users.noreply.github.com>
2020-08-21 15:06:09 -0400

    Add download button, support for Google Scholar
    - Implement a button to make a GET request to the Scholarcy API for a .RIS file of references from the input URL
    - Implement support for Google Scholar links when choosing 'Display results', with an option to enable or disable this feature
    - Small design tweaks

--- js/analyzer.js
+++ js/analyzer.js
@@ -5,21 +5,32 @@ displayButton.onclick = function() {
   displayResults();
   return false;
 }
+var downloadButton = document.getElementById('download');
+download.onclick = function() { 
+  downloadResults();
+  return false;
+}
 function displayResults() { 
     var form = document.getElementById("paperinfo");
     var url = form.elements[0].value;
     var document_type = form.elements[1].value;
     var reference_style = form.elements[2].value; 
+    document.getElementById("results").innerHTML = "Loading! Please wait..."
+    var scholarLinksEnabled = form.elements[3].value === "yes" ? true : false; 
     (async () => {
     try {
       var response = await fetch("https://ref.scholarcy.com/api/references/extract?url=" + url + "&document_type=" + document_type + "&resolve_references=true&reference_style=" + reference_style + "&engine=v1");
       var data = await response.json();
-      for (reference of data.references) {
-        document.getElementById("results").innerHTML += reference + "<br><br>";
+      document.getElementById("results").innerHTML = "";
+      for (reference of data.reference_links) {
+        document.getElementById("results").innerHTML += reference.entry
+        if (scholarLinksEnabled && reference.scholar_url) { 
+          document.getElementById("results").innerHTML += "<a href=" + reference.scholar_url + " target= _blank> <img border='0' alt='Google Scholar' src='images/scholar.png' width='20' height='20'></a>";
+        }
+        document.getElementById("results").innerHTML += "<br><br>";
       }
     } catch (e) {
       console.log('Error', e);
-      console.log(url);
       if (url == "") {
         document.getElementById("results").innerHTML = "Enter a URL for me to analyze!";
       } else {
@@ -31,5 +42,30 @@ function displayResults() {
 
 function clearOutput() {
   document.getElementById("results").innerHTML = "";
+}
 
+function downloadResults() { 
+  var form = document.getElementById("paperinfo");
+  var url = form.elements[0].value;
+  var document_type = form.elements[1].value;
+  var reference_style = form.elements[2].value; 
+  var scholarLinksEnabled = form.elements[3].value === "yes" ? true : false; 
+  (async () => {
+  try {
+    var response = await fetch("https://ref.scholarcy.com/api/references/download?url=" + url + "&document_type=" + document_type + "&reference_style=" + reference_style + "&reference_format=ris&engine=v1");
+    console.log(response);
+    if (response.status === 200) {
+      window.open(response.url);
+    } else { 
+      document.getElementById("results").innerHTML = "An error was encountered with the file you submitted. Please try again in a few moments or email me at nicasnes@gmail.com."
+    }
+  } catch (e) {
+    console.log('Error', e);
+    if (url == "") {
+      document.getElementById("results").innerHTML = "Enter a URL for me to analyze!";
+    } else {
+      document.getElementById("results").innerHTML = "An error was encountered with the file you submitted. Please try another file or email me at nicasnes@gmail.com."
+    }
+  }
+})();
 }
